02208123cfd81966b7c2c158e9c4faabbaddb0cb
SirCypkowskyy <cypkowski@gmail.com>
2021-06-15 15:47:07 +0200

    Update #2
    - Basic setup for AgentController.cs
    - Expansion of LevelController.cs (spawn agents logic buildup)
    - Basic TaskScene setup

--- 7elements-unity-project/Assets/Scripts/AgentController.cs
+++ 7elements-unity-project/Assets/Scripts/AgentController.cs
@@ -1,18 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class AgentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Agent Parameters & Setup
+    private NavMeshAgent thisNavMeshAgent;
+    private LevelController levelControllerInstance;
+    private Color agentColor;
+    private Renderer agentRenderer;
+    [Header("Agent Health & Setup")]
+    [SerializeField] private float destinationTolerance = 1f;
+    [SerializeField] private int agentHealth = 3;
+
+    private float oneSecondUpdateTimer = 1f;
+    #endregion
+
+    private void Awake()
     {
-        
+        thisNavMeshAgent = GetComponent<NavMeshAgent>();
+        Color[] colors = {
+        Color.blue,
+        Color.black,
+        Color.red,
+        Color.yellow};
+        int randomColor = Random.Range(0, colors.Length);
+        agentColor = colors[randomColor];
+        agentRenderer =gameObject.GetComponent<Renderer>();
+        agentRenderer.material.SetColor($"{gameObject.name}_Color", agentColor);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        levelControllerInstance = LevelController.instance;
+        this.gameObject.tag = "LevelAgent";
+
     }
+    private void Update()
+    {
+        if (thisNavMeshAgent.destination == null)
+        {
+            thisNavMeshAgent.destination = FindNewDestination(new Vector3(0, 0, 0));
+        }
+        else 
+        {
+            if (oneSecondUpdateTimer > 0)
+                oneSecondUpdateTimer -= Time.deltaTime;
+            else
+            {
+                OneSecondUpdate();
+            }
+        }
+    }
+
+    private void OneSecondUpdate()
+    {
+        if (Vector3.Distance(transform.position, thisNavMeshAgent.destination) < destinationTolerance)
+        {
+            Vector3 previousDestination = thisNavMeshAgent.destination;
+            thisNavMeshAgent.destination = FindNewDestination(previousDestination);
+        }
+        oneSecondUpdateTimer = 1f;
+    }
+
+    private Vector3 FindNewDestination(Vector3 oldPosition)
+    {
+        float[] board_size = levelControllerInstance.ReturnBoardSize();
+        float offsetFromBoundaries = 0.5f;
+        Vector3 newPosition = new Vector3(
+            Random.Range(-(board_size[0]) + offsetFromBoundaries, board_size[0] - offsetFromBoundaries),
+            0,
+            Random.Range(-(board_size[1]) + offsetFromBoundaries, board_size[1] - offsetFromBoundaries)
+            );
+        while (oldPosition == newPosition)
+        {
+            newPosition = new Vector3(
+            Random.Range(-(board_size[0]) + offsetFromBoundaries, board_size[0] - offsetFromBoundaries),
+            0,
+            Random.Range(-(board_size[1]) + offsetFromBoundaries, board_size[1] - offsetFromBoundaries)
+            );
+        }
+
+        return newPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("LevelAgent"))
+        {
+            LoseHealth();
+            Debug.Log($"{gameObject.name} lost one health point.\nCurrent health: {agentHealth}");
+        }
+    }
+
+    public void LoseHealth()
+    {
+        agentHealth -= 1;
+    }
+
+    public int GetAgentHealth()
+    {
+        return agentHealth;
+    }
+
+  
 }
--- 7elements-unity-project/Assets/Scripts/LevelController.cs
+++ 7elements-unity-project/Assets/Scripts/LevelController.cs
@@ -7,11 +7,20 @@ public class LevelController : MonoBehaviour
     #region Level Parameters
     [Header("Used agent prefab")]
     [SerializeField] private GameObject agentPrefab;
+    [Header("Board")]
+    [SerializeField] private GameObject boardOnScene;
     [Header("New agent appearence time setup")]
     [Range(2, 10)] [SerializeField] private float newAgentMinEmergeneTime; 
     [Range(2, 10)][SerializeField] private float newAgentMaxEmergenceTime; 
     [Header("Maximum number of agents on level")]
     [Range(1, 30)] [SerializeField] private int maxAmmountofAgentsOnLevel;
+
+    //Board size
+    private float boardSizeX;
+    private float boardSizeZ;
+
+    //Spawn another agent
+    private float timeToSpawnNewAgent;
     #endregion
 
     #region LevelController Instance Setup
@@ -40,4 +49,41 @@ public class LevelController : MonoBehaviour
         }
     }
     #endregion
+
+    private void Start()
+    {
+        DetermineBoardSize();
+        timeToSpawnNewAgent = Random.Range(newAgentMinEmergeneTime, newAgentMaxEmergenceTime);
+    }
+
+    private void DetermineBoardSize()
+    {
+        boardSizeX = boardOnScene.transform.position.x;
+        boardSizeZ = boardOnScene.transform.position.z;
+    }
+
+    public float[] ReturnBoardSize()
+    {
+        float[] localList = new float[2];
+        localList[0] = boardSizeX;
+        localList[1] = boardSizeZ;
+        return localList;
+    }
+
+    private void Update()
+    {
+        if (timeToSpawnNewAgent > 0)
+        {
+            timeToSpawnNewAgent -= Time.deltaTime;
+        }
+        else
+        {
+            SpawnNewAgent();
+        }
+    }
+
+    private void SpawnNewAgent()
+    {
+        throw new System.NotImplementedException();
+    }
 }
